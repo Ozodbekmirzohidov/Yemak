70621d7e1592f6e35fc4bcdb1576d83d204abf8b
Ozodbek Mirzohidov <ozodbek.mirzohidov@icloud.com>
2026-04-01 11:39:38 +0500

    feat: complete RestaurantCard component

---
+++ src/components/RestaurantCard/RestaurantCard.jsx
@@ -0,0 +1,17 @@
+export function RestaurantCard({ img, name, tags }) {
+  return (
+    <li className="w-[344px] rounded-[16px] pb-[16px] overflow-hidden border border-[#F0F0F0] bg-[#FFFFFF] cursor-pointer">
+      <div className="w-full h-[180px] mb-[12px] overflow-hidden rounded-[16px] ">
+        <img className="w-full h-full" src={img} alt={name} />
+      </div>
+      <div>
+        <div className="ml-[12px] text-16px font-bold text-[#12282F] mb-[6px]">
+          {name}
+        </div>
+        <div className="ml-[12px] font-semibold text-[14px] text-[#5A696E]">
+          {tags?.join(", ")}
+        </div>
+      </div>
+    </li>
+  );
+}
